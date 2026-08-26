5291c5d79758dd65be16ca49a5b9f829e5393b89
metanetabasova <metanetabasova314@gmail.com>
2026-08-26 16:48:08 +0000

    Add

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -1,11 +1,24 @@
 #!/usr/bin/python3
+import sys
+
 def fizzbuzz(n):
     for i in range(1, n + 1):
         if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz", end=" ")
+            print("FizzBuzz", end="")
         elif i % 3 == 0:
-            print("Fizz", end=" ")
+            print("Fizz", end="")
         elif i % 5 == 0:
-            print("Buzz", end=" ")
+            print("Buzz", end="")
         else:
-            print(i, end=" ")
+            print(i, end="")
+        
+
+        if i != n:
+            print(" ", end="")
+    print()
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        fizzbuzz(int(sys.argv[1]))
+    else:
+        fizzbuzz(89)
