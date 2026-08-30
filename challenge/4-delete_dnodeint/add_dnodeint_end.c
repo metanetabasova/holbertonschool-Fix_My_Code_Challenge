bc358359ae6c56e038b8c95d1ba3c35acac956e1
metanetabasova <metanetabasova314@gmail.com>
2026-08-30 14:38:55 +0000

    Add

---
+++ challenge/4-delete_dnodeint/add_dnodeint_end.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include "lists.h"
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node, *last;
+
+	if (!head)
+		return (NULL);
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		new_node->prev = NULL;
+		*head = new_node;
+		return (new_node);
+	}
+
+	last = *head;
+	while (last->next != NULL)
+		last = last->next;
+
+	last->next = new_node;
+	new_node->prev = last;
+
+	return (new_node);
+}
